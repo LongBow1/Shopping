f1534f5ea76cd7a4cbda849dfc491a38c86392e3
LongBow <special_for_reg@163.com>
2019-12-02 01:06:01 +0800

    update selltype desc

--- auto-shopping/src/main/java/com/myqq/service/youza/bll/AutoShoppingEntry.java
+++ auto-shopping/src/main/java/com/myqq/service/youza/bll/AutoShoppingEntry.java
@@ -197,7 +197,13 @@ public class AutoShoppingEntry {
                     tmpToBuy.setGoodSpecKeyWords(Arrays.asList(intendOrderInfo.getGoodSpecList().split("\\,")));
                     descSB.append(" 规格:").append(intendOrderInfo.getGoodSpecList());
                 }
-                descSB.append(" 预售:").append(intendOrderInfo.isNeedPresale() ? "是":"否").append(" 时间:").append(tmpToBuy.getOrderDate());
+                String preSellDesc = "不限";
+                if(intendOrderInfo.getToBuySellType() == 1){
+                    preSellDesc = "现货";
+                }else if(intendOrderInfo.getToBuySellType() == 2){
+                    preSellDesc = "预售";
+                }
+                descSB.append(" 预售:").append(preSellDesc).append(" 时间:").append(tmpToBuy.getOrderDate());
                 tmpToBuy.setDesc(descSB.toString());
             });
         }
