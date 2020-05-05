b6274fac76d0f550c88a00ccfaa7055a9305958d
LongBow <special_for_reg@163.com>
2020-05-05 20:22:02 +0800

    update timeformat

--- auto-shopping/src/main/java/com/myqq/service/youza/util/TimeUtil.java
+++ auto-shopping/src/main/java/com/myqq/service/youza/util/TimeUtil.java
@@ -4,9 +4,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
-    public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss ");
+    public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss SSS");
 
     public static String getCurrentTimeString(){
         return dateTimeFormatter.format(LocalDateTime.now())+" ";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCurrentTimeString());
     }
 }
